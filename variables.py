37cfad3890d496d7a6698ffd38d3931ad2671b6e
fridah51 <fridahshiku1@gmail.com>
2020-03-10 17:19:58 +0300

    var,slicin,index

---
+++ variables.py
@@ -0,0 +1,13 @@
+name ="Fridah wanjiku"
+number =2
+student =True
+married =False
+
+print(name)
+print(type(number))
+print(type(student))
+print(married)
+
+school="TECH CAMP"
+
+print(school[-4:])
